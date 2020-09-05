16950ee2ed0a3edb0ef25f2045ad9fbf115c2d4b
Xiaolong Lin <xxl5453@psu.edu>
2020-09-05 03:23:46 +0000

    02

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <readline/readline.h>
 #include <stdlib.h>
 int main(void) {
-  char*tempstr = readline("Enter temperature in celsius:");
+  char*tempstr = readline("Enter temperature in celsius: ");
   double temp = atof(tempstr);
   double F=temp*1.8+32;
 
