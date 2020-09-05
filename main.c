f7330ebf03e903d36feaad1357d06803092a03d5
Xiaolong Lin <xxl5453@psu.edu>
2020-09-05 03:09:05 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <readline/readline.h>
+#include <stdlib.h>
+int main(void) {
+  char*tempstr = readline("Enter temperature in Celsius:");
+  double temp = atof(tempstr);
+  double F=temp*1.8+32;
+
+
+  printf("%f° in Celsius is equivalent to %f° Fahrenheit.\n",temp,F );
+  
+  return 0;
+}
